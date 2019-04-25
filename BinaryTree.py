348b22b5fcca2ae2d943ec7e1cd397b3381b3d38
gr8rajas <gr8rajas@gmail.com>
2019-04-25 09:53:07 -0500

    Create BinaryTree.py

---
+++ BinaryTree.py
@@ -0,0 +1,38 @@
+class BTree:
+    def __init__(self,data):
+        self.left  = None
+        self.right = None
+        self.data  = data
+        
+    def printer(self):
+        print(self.data)
+        
+    def insert(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = BTree(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = BTree(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+# Use the insert method to add nodes
+root = BTree(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+    
+root.PrintTree() 
